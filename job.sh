85ec7ed7920ed97c29224f05157569162f3d5003
wcl20 <alvislee2020@gmail.com>
2020-11-15 23:25:46 +0800

    Added directories in job.sh

--- job.sh
+++ job.sh
@@ -2,6 +2,8 @@
 
 # Remove all feature dataset
 rm features/*
+mkdir features
+mkdir output
 
 python3 features_extraction.py -d ../datasets/food5k/training -o features/training_features.hdf5
 python3 features_extraction.py -d ../datasets/food5k/validation -o features/validation_features.hdf5 --pca models/pca.pkl --scaler models/scaler.pkl
